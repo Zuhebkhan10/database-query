97f4798f5ff84a43918ad9ac7c96d3a4baed9297
Zuheb khan <zuhebk433@gmail.com>
2026-02-02 21:33:26 +0530

    database commit

---
+++ String-function.sql
@@ -0,0 +1,26 @@
+Create table customer(
+cust_id int primary key,
+cust_name varchar(25),
+company varchar(24),
+address varchar(25),
+contact bigint
+);
+
+-- Rename the column name is address to "Country"
+Alter table customer
+rename column address to country;
+
+insert into customer(cust_id,cust_name,company,address,contact) values
+(101,"Mike",'Amazon','USA',98764345678),
+(102,'Shahzain','Paytm','India',9876456721),
+(103,'Arzan','Oneplus','China',8889876567);
+
+Select *from customer;
+-- print the customer name is Uppercase
+Select cust_name,Ucase(cust_name) as uppercase from customer;
+-- print the customer name is lowercase
+Select company, Lcase(company) as lowercase from customer;
+
+-- merge or concatenate cust_name and company name as full name is customer table.
+Select cust_name, company,country,concat(cust_name ,",",country)as names from customer;
+
--- where-condition.sql
+++ where-condition.sql
@@ -46,4 +46,59 @@ select *from employee
  select *from employee
  where salary between 90000 and 100000;
  
+ SELECT * FROM employee;
+
+-- select *from students;
+-- Get the records of teg student whose nam e statrt with letter "A"
+-- Use the "LIKE" logical operator and to mmatc with the stirng use the "%"
+Select *from employee where emp_name like "A%";
+
+-- Don't know first 2 letters and next letter is "e" get the records of the employee for the unknown letter use 
+Select *from employee where emp_name like "__e%";
+
+-- Get the record of emp_id is 101,102,106-- 
+-- In a logical operator used to check multiple records at a time. 
+Select *from employee where emp_id In(101,102,106);
+ 
+-- get all the records of the employee expect 101,102,105
+select * from employee where emp_id Not in (101,104,105);
+
+-- Add a column "city" to the table employee-- 
+Alter table employee add column city varchar(25);
+Select *from employee;
+
+-- how to update the null values
+-- To update the null we use DML command--> Update
+
+Set Sql_safe_updates=0;
+-- Before the updating the null vaues in the table disable security safe update.
+update employee set city="Pune"; 
+select *from employee;
+
+
+-- Enable secirty safe updates
+set Sql_safe_update=1;
+
+Delete from employee where emp_id=108;
+-- Note:if we want add a new column, remove a columns and remove a record.
+-- We need to disable the security safe updates
+
+-- Transaction are nothing nut set of quries
+-- TCL commands control transactions by committing or rolling back changes in a database.
+Start Transaction ;
+Alter table employee_info drop column city;
+Select *from employee_info;
+Rollback;
+
+Select *from employee_info;
+Start transaction;
+Delete from employee_info where emp_id=105;
+Rollback;
+Select *from employee_info;
+commit;
+
+-- how to rename the table name use mysql
+Rename table emmploye_info to employee_info;
+
+
 
